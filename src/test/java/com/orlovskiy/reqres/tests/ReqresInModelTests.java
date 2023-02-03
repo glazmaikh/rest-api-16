5891f7aa673224692201017860d185e826ceb364
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2023-02-03 15:57:41 +0300

    added tag api_test

--- src/test/java/com/orlovskiy/reqres/tests/ReqresInModelTests.java
+++ src/test/java/com/orlovskiy/reqres/tests/ReqresInModelTests.java
@@ -3,6 +3,7 @@ package com.orlovskiy.reqres.tests;
 import com.orlovskiy.reqres.TestBase;
 import com.orlovskiy.reqres.models.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,6 +19,7 @@ import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@Tag("api")
 public class ReqresInModelTests extends TestBase {
 
     @ValueSource(strings = {"Lindsay", "Michael", "Tobias", "Byron", "George", "Rachel"})
