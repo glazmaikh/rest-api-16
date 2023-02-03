99704f9f1c05dada053f1cc11d67fa147c0b563c
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2023-02-03 13:56:04 +0300

    added selenideListener to TestBase

--- src/test/java/com/orlovskiy/reqres/TestBase.java
+++ src/test/java/com/orlovskiy/reqres/TestBase.java
@@ -1,8 +1,12 @@
 package com.orlovskiy.reqres;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -12,5 +16,15 @@ public class TestBase {
         RestAssured.basePath = "/api";
         //System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver.exe");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+
     }
 }
